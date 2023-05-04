2df8532921cd4b8e2e6e6b5e0b297126def4b133
Dorian-Mrll <54944480+Dorian-Mrll@users.noreply.github.com>
2023-05-04 17:51:46 +0200

    Add files via upload

--- Adafruit/src/main.c
+++ Adafruit/src/main.c
@@ -1,136 +1,136 @@
-#include <zephyr.h>
-#include <device.h>
-#include <devicetree.h>
-#include <drivers/gpio.h>
+// #include <zephyr.h>
+// #include <device.h>
+// #include <devicetree.h>
+// #include <drivers/gpio.h>
 
-#include <string.h>
-#include <drivers/uart.h>
-#include <drivers/i2c.h>
-
-
-#define GPS_ADR 0x42
-#define BUFFER_SIZE 256
-
-// The devicetree node identifier for the "led0" alias.
-#define GPS_NODE DT_ALIAS(gps)
+// #include <string.h>
+// #include <drivers/uart.h>
+// #include <drivers/i2c.h>
 
 
-#if DT_NODE_HAS_STATUS(GPS_NODE, okay)
-#define GPS		DT_GPIO_LABEL(GPS_NODE, gpios)
-#define PIN	    DT_GPIO_PIN(GPS_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(GPS_NODE, gpios)
-#else
-// A build error here means your board isn't set up to blink an LED.
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define GPS	""
-#define PIN	18
-#define FLAG GPIO_OUTPUT_ACTIVE
-#endif
+// #include <stdio.h>
+
+// #define I2C_ADDR_MIN 0x08 // Adresse minimale à scanner
+// #define I2C_ADDR_MAX 0x77 // Adresse maximale à scanner
+// //#define GPS_ADR 0x42
+// //#define BUFFER_SIZE 256
+
+// // The devicetree node identifier for the "led0" alias.
+// #define GPS_NODE DT_ALIAS(gps)
 
 
-void main(void)
-{
-
-	// Activer broche 18
-	const struct device * gps_dev;
-	int ret;
-
-	gps_dev = device_get_binding(GPS);
-	if (gps_dev == NULL) {
-  		printk("Failed to get GPS device binding.\n");
-	}
-
-	ret = gpio_pin_configure(gps_dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if (ret != 0) {
-  		printk("Failed to configure GPS device binding.\n");
-	}
-
-	ret = gpio_pin_set(gps_dev, PIN, true);
-	if (ret != 0) {
-  		printk("Failed to configure GPS device binding.\n");
-	}
-
-
-	const char * res4 = DT_PROP(DT_ALIAS(gps), label);
-	printk("label ? %s\n", res4);
-
-	// Ouvrir l'I2C
-	const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(sercom3));
-
-
-	if (i2c_dev == NULL) {
-  		printk("Failed to get I2C device binding.\n");
-	}
-
-
-	if(!device_is_ready(i2c_dev)){
-		printk("I2C bus %s is not ready !\n\r", i2c_dev->name);
-	}
-	printk("I2C bus %s is ready !\n\r", i2c_dev->name);
-
-
-
-
-	
-	
-	ret = i2c_configure(i2c_dev, I2C_SPEED_FAST);
-    if (ret != 0) {
-        printk("Failed to configure I2C device\n");
-        return;
-    }
-
-    ret = i2c_reg_write_byte(i2c_dev, GPS_ADR, 0x00, 0x01);
-    if (ret != 0) {
-        printk("GPS not detected\n");
-        return;
-    }
-
-    printk("GPS detected\n");
+// #if DT_NODE_HAS_STATUS(GPS_NODE, okay)
+// #define GPS		DT_GPIO_LABEL(GPS_NODE, gpios)
+// #define PIN	    DT_GPIO_PIN(GPS_NODE, gpios)
+// #define FLAGS	DT_GPIO_FLAGS(GPS_NODE, gpios)
+// #else
+// // A build error here means your board isn't set up to blink an LED.
+// #error "Unsupported board: led0 devicetree alias is not defined"
+// #define GPS	""
+// #define PIN	18
+// #define FLAG GPIO_OUTPUT_ACTIVE
+// #endif
 
 
 
 
 
+// // uint32_t i2c_cfg = I2C_SPEED_SET(I2C_SPEED_FAST) | I2C_MODE_MASTER;
+
+// void main(void)
+// {
+// 	// Activer broche 18
+// 	const struct device * gps_dev;
+// 	int ret;
+
+// 	gps_dev = device_get_binding(GPS);
+// 	if (gps_dev == NULL) {
+//   		printk("Failed to get GPS device binding.\n");
+// 	}
+
+// 	ret = gpio_pin_configure(gps_dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+// 	if (ret != 0) {
+//   		printk("Failed to configure GPS device binding.\n");
+// 	}
+
+// 	ret = gpio_pin_set(gps_dev, PIN, true);
+// 	if (ret != 0) {
+//   		printk("Failed to configure GPS device binding.\n");
+// 	}
 
 
-	/*if(i2c_write(dev_sercom_3, "0xFD", sizeof("0xFD"), 0x42) != 0){
-		printk("Error i2c_write.\n");
-	}
-	printk("Write\n\r");
 
-	while(true){
-		if(i2c_read(dev_sercom_3, buf, sizeof(buf), 0x42) != 0){
-			printk("Fail to fetch sample from slave. \n");
-		}
-		else	printk("buf=%x\n", buf[0]);
+//     k_sleep(K_MSEC(5000));
 
 
+// // 	const char * res4 = DT_PROP(DT_ALIAS(gps), label);
+// // 	printk("label ? %s\n", res4);
 
-	}*/
-
-	/*uint8_t read_data[BUFFER_SIZE]; // Example buffer for storing GNSS/GPS data
-	ret = i2c_read(dev_sercom_3, read_data, sizeof(read_data), GPS_ADR);
-	if (ret != 0) {
-  		printk("Failed to read I2C data. Error : %d\n", ret);
-	}
+// // 	// Ouvrir l'I2C
+// // 	const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(sercom3));
 
 
-	for(int i=0; i<BUFFER_SIZE; i++){
+// // 	if (i2c_dev == NULL) {
+// //   		printk("Failed to get I2C device binding.\n");
+// // 	}
 
-		printk("Data : %d\n", read_data[i]);
-	}
+
+// // 	if(!device_is_ready(i2c_dev)){
+// // 		printk("I2C bus %s is not ready !\n\r", i2c_dev->name);
+// // 	}
+// // 	printk("I2C bus %s is ready !\n\r", i2c_dev->name);
 
 
-	int res2 = DT_NODE_HAS_PROP(DT_NODELABEL(sercom_3), clock_frequency);
-	printk("clock_frequency ? %d\n", res2);
-	
-	res2 = DT_PROP(DT_ALIAS(sercom_3), clock_frequency);
-	printk("clock_frequency ? %d\n", res2);
 
+
+// // 	ret = i2c_configure(i2c_dev, i2c_cfg);
+// //     if (ret != 0) {
+// //         printk("Failed to configure I2C device : %d\n", ret);
+// //         return;
+// //     }
+// // 	printk("Configure I2C device : %d\n", ret);
+
+
+
+// // 	unsigned char datas[6];
 
 	
-	int res3 = DT_NODE_HAS_PROP(DT_NODELABEL(sercom_3), label);
-	printk("label ? %d\n", res3);*/
+
+// // 	datas[0] = 0x01;
+// // 	datas[1] = 0x20;
+
+// // 	/* 3. verify i2c_write() */
+// // 	if (i2c_write(i2c_dev, datas, 2, GPS_ADR)) {
+// // 		printk("Fail to configure sensor GY271\n");
+// // 		return;
+// // 	}
+
+// // 	datas[0] = 0x02;
+// // 	datas[1] = 0x00;
+// // 	if (i2c_write(i2c_dev, datas, 2, GPS_ADR)) {
+// // 		printk("Fail to configure sensor GY271\n");
+// // 		return;
+// // 	}
+
+// // 	k_sleep(K_MSEC(1));
+
+// // 	datas[0] = 0x03;
+// // 	if (i2c_write(i2c_dev, datas, 1, GPS_ADR)) {
+// // 		printk("Fail to write to sensor GY271\n");
+// // 		return;
+// // 	}
+
+// // 	(void)memset(datas, 0, sizeof(datas));
+
+// // 	/* 4. verify i2c_read() */
+// // 	if (i2c_read(i2c_dev, datas, 6, GPS_ADR)) {
+// // 		printk("Fail to fetch sample from sensor GY271\n");
+// // 		return;
+// // 	}
+
+// // 	printk("axis raw data: %d %d %d %d %d %d\n",
+// // 				datas[0], datas[1], datas[2],
+// // 				datas[3], datas[4], datas[5]);
 
 
-}
+// // }
