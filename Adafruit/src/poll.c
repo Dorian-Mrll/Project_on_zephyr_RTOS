ffcacde256bafafdda8eca83df9d90a05a074cbf
Dorian-Mrll <54944480+Dorian-Mrll@users.noreply.github.com>
2023-05-12 16:20:23 +0200

    Add files via upload

--- Adafruit/src/poll.c
+++ Adafruit/src/poll.c
@@ -1,132 +1,190 @@
-//  #include <zephyr.h>
-//  #include <device.h>
-//  #include <devicetree.h>
-//  #include <drivers/gpio.h>
-// #include <drivers/uart.h>
-
-// #define SARAENABLE_NODE DT_ALIAS(saraenable)
-
-// #if DT_NODE_HAS_STATUS(SARAENABLE_NODE, okay)
-// #define SARAENABLE	DT_GPIO_LABEL(SARAENABLE_NODE, gpios)
-// #define PIN         DT_GPIO_PIN(SARAENABLE_NODE, gpios)
-// #define FLAGS	    DT_GPIO_FLAGS(SARAENABLE_NODE, gpios)
-// #else
-// // A build error here means your board isn't set up to blink an LED.
-// #error "Unsupported board: led0 devicetree alias is not defined"
-// #define SARAENABLE	""
-// #define PIN	4
-// #define FLAG GPIO_OUTPUT_ACTIVE
-// #endif
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+#include <drivers/uart.h>
 
 
-// #define SARATOGGLE_NODE DT_ALIAS(saratoggle)
+#include <string.h>
 
-// #if DT_NODE_HAS_STATUS(SARATOGGLE_NODE, okay)
-// #define SARATOGGLE  DT_GPIO_LABEL(SARATOGGLE_NODE, gpios)
-// #define PIN2	    DT_GPIO_PIN(SARATOGGLE_NODE, gpios)
-// #define FLAGS2	    DT_GPIO_FLAGS(SARATOGGLE_NODE, gpios)
-// #else
-// // A build error here means your board isn't set up to blink an LED.
-// #error "Unsupported board: led0 devicetree alias is not defined"
-// #define SARATOGGLE	""
-// #define PIN	10
-// #define FLAG GPIO_OUTPUT_ACTIVE
-// #endif
+#define SARAENABLE_NODE DT_ALIAS(saraenable)
 
-
-
-// void main(void) {
+#if DT_NODE_HAS_STATUS(SARAENABLE_NODE, okay)
+#define SARAENABLE	DT_GPIO_LABEL(SARAENABLE_NODE, gpios)
+#define PIN         DT_GPIO_PIN(SARAENABLE_NODE, gpios)
+#define FLAGS	    DT_GPIO_FLAGS(SARAENABLE_NODE, gpios)
+#else
+// A build error here means your board isn't set up to blink an LED.
+#error "Unsupported board: led0 devicetree alias is not defined"
+#define SARAENABLE	""
+#define PIN	4
+#define FLAG GPIO_OUTPUT_ACTIVE
+#endif
 
 
-//     int ret;
+#define SARATOGGLE_NODE DT_ALIAS(saratoggle)
 
-// 	// Activer broche 4 ET 10
-// 	const struct device * saraenable_dev;
-//     const struct device * saratoggle_dev;
-
-// 	saraenable_dev = device_get_binding(SARAENABLE);
-// 	if (saraenable_dev == NULL) {
-//   		printk("Failed to get GPS device binding.\n");
-// 	}
-// 	ret = gpio_pin_configure(saraenable_dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-// 	if (ret != 0) {
-//   		printk("Failed to configure GPS device binding.\n");
-// 	}
-// 	ret = gpio_pin_set(saraenable_dev, PIN, true);
-// 	if (ret != 0) {
-//   		printk("Failed to configure GPS device binding.\n");
-// 	}
+#if DT_NODE_HAS_STATUS(SARATOGGLE_NODE, okay)
+#define SARATOGGLE  DT_GPIO_LABEL(SARATOGGLE_NODE, gpios)
+#define PIN2	    DT_GPIO_PIN(SARATOGGLE_NODE, gpios)
+#define FLAGS2	    DT_GPIO_FLAGS(SARATOGGLE_NODE, gpios)
+#else
+// A build error here means your board isn't set up to blink an LED.
+#error "Unsupported board: led0 devicetree alias is not defined"
+#define SARATOGGLE	""
+#define PIN2	10
+#define FLAG GPIO_OUTPUT_ACTIVE
+#endif
 
 
-//     saratoggle_dev = device_get_binding(SARATOGGLE);
-// 	if (saratoggle_dev == NULL) {
-//   		printk("Failed to get GPS device binding.\n");
-// 	}
-// 	ret = gpio_pin_configure(saratoggle_dev, PIN2, GPIO_OUTPUT_ACTIVE | FLAGS2);
-// 	if (ret != 0) {
-//   		printk("Failed to configure GPS device binding.\n");
-// 	}
-// 	ret = gpio_pin_set(saratoggle_dev, PIN2, false);
-// 	if (ret != 0) {
-//   		printk("Failed to configure GPS device binding.\n");
-// 	}
+const struct uart_config uart_cfg = {
+		.baudrate = 115200,
+		.parity = UART_CFG_PARITY_NONE,
+		.stop_bits = UART_CFG_STOP_BITS_1,
+		.data_bits = UART_CFG_DATA_BITS_8,
+		.flow_ctrl = UART_CFG_FLOW_CTRL_NONE
+};
 
 
 
-//     k_sleep(K_MSEC(2000));
-
-//     ret = gpio_pin_configure(saratoggle_dev, PIN2, GPIO_INPUT | FLAGS2);
-// 	if (ret != 0) {
-//   		printk("Failed to configure GPS device binding.\n");
-// 	}
-
-//     k_sleep(K_MSEC(1000));
+void main(void) {
 
 
+    int ret;
+
+	// Activer broche 4 ET 10
+	const struct device * saraenable_dev;
+    const struct device * saratoggle_dev;
+
+	saraenable_dev = device_get_binding(SARAENABLE);
+	if (saraenable_dev == NULL) {
+  		printk("Failed to get GPS device binding.\n");
+	}
+	ret = gpio_pin_configure(saraenable_dev, PIN, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}
+    k_sleep(K_MSEC(1000));
+	ret = gpio_pin_set(saraenable_dev, PIN, true);
+	if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}
+    k_sleep(K_MSEC(1000));
 
 
+    saratoggle_dev = device_get_binding(SARATOGGLE);
+	if (saratoggle_dev == NULL) {
+  		printk("Failed to get GPS device binding.\n");
+	}
+	ret = gpio_pin_configure(saratoggle_dev, PIN2, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}
+    ret = gpio_pin_set(saratoggle_dev, PIN2, false);
+	if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}
 
+    k_sleep(K_MSEC(1000));
 
+    ret = gpio_pin_configure(saratoggle_dev, PIN2, GPIO_INPUT);
+	if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}
 
-
-
-//     const char *dev_name = DT_LABEL(DT_ALIAS(sercom_5));
-//     const char *dev_name2 = DT_LABEL(DT_ALIAS(sercom_2));
-
-//     const struct device *uart_dev1;
-//     const struct device *uart_dev2;
+    k_sleep(K_MSEC(2000));
 
 
 
 
-//     uart_dev1 = device_get_binding(dev_name);
-//     if (!uart_dev1) {
-//         printk("Cannot get UART device 1\n");
-//     }
 
-//     uart_dev2 = device_get_binding(dev_name2);
-//     if (!uart_dev2) {
-//         printk("Cannot get UART device 2\n");
-//     }
+    const struct device *uart_dev1 = DEVICE_DT_GET(DT_NODELABEL(sercom5));
+    const struct device *uart_dev2 = DEVICE_DT_GET(DT_NODELABEL(sercom2));
+
+    if (!uart_dev1) {
+        printk("Cannot get UART device 1\n");
+    }
+
+    if (!uart_dev2) {
+        printk("Cannot get UART device 2\n");
+    }
+
+    /*ret = uart_configure(uart_dev1, &uart_cfg);
+    if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}*/
+    /*
+    ret = uart_configure(uart_dev2, &uart_cfg);
+    if (ret < 0) {
+  		printk("Failed to configure GPS device binding.\n");
+	}*/
+    k_sleep(K_MSEC(2000));
 
     
-//     char buffer[0];
+    uint8_t buffer[0];
 
-//     const char *msg = "AT";
+    const uint8_t msg [] = "AT+CGMR";
 
-//     for (int i = 0; i < sizeof(msg); i++) {
-//         uart_poll_out(uart_dev2, msg[i]);
-//     }
+    for (int i = 0; i < sizeof(msg); i++) {
+        uart_poll_out(uart_dev2, msg[i]);
+        k_sleep(K_MSEC(500));
+    }
 
-//     while (1) {
+    //uart_poll_out(uart_dev2, 0x0D);
+    //uart_poll_out(uart_dev2, 0x0A);
+
+    //uart_poll_out(uart_dev2, (uint8_t)0x44);
+
+
+    uint8_t buf[20];
+    memset(buf, 0, sizeof(buf));
+    k_sleep(K_MSEC(1000));
+
+
+
+    //while (1) {
         
-//         //Reception des données
-//         int ret = uart_poll_in(uart_dev2, buffer);
-//         if (ret != 0) {
-//             printk("Failed to send data over UART\n");
-//         }
-//         else{
-//             printk("Received data: %c\n", buffer[0]);
-//         }
-//     }
-// }
+        // //Reception des données
+        // int ret = uart_poll_in(uart_dev2, &buffer[0]);
+        // if (ret < 0) {
+        //     printk("Failed to send data over UART\n");
+        // }
+        // else{
+        //     printk("Received data: %c\n", buffer[0]);
+        // }
+        // k_sleep(K_MSEC(2000));
+        
+        
+
+
+        for(int i=0; i<20; i++){
+
+            ret = uart_poll_in(uart_dev2, &buf[i]);
+
+            if (ret < 0) {
+                printk("Failed to send data over UART: %d\n", ret);
+            } /*else {
+                
+                printk("%c", buf[i]);
+            }*/
+        
+        }
+        k_sleep(K_MSEC(1000));
+
+        printk("Received data: %s\n", buf);
+
+            
+            
+            
+        int cpt=0;
+        
+        for(int i=0; i<20; i++){
+
+            if(buf[i] != 0) cpt++;
+            else break;
+        
+        }
+        printk("Received data: %d\n", cpt);
+
+    //}
+}
